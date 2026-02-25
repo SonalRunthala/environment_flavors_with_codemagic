6c1bdeaee75bbfaf67217f3f9a2571f8ae63b1f9
SonalSharma <sonalsharma.img@gmail.com>
2026-02-25 18:40:41 +0530

    setup all removed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,10 +12,10 @@ import 'package:testtwentysix/app_widget.dart';
 import 'package:testtwentysix/main_common.dart';
 
 void main() {
-  setUpAll(() {
-    // This setup code has an error
-    throw Exception("Failed to initialize something critical for tests");
-  });
+  // setUpAll(() {
+  //   // This setup code has an error
+  //   throw Exception("Failed to initialize something critical for tests");
+  // });
 
   test("1+1 == 2", () {
     // This test will never even run because setUpAll failed.
